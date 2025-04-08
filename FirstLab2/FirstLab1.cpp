b9a87e99ab393ffa569a707ba2b54309374c3d19
flower_boii <nikit_den@mail.ru>
2025-04-08 20:06:01 +0500

    Added first task in first lab

---
+++ FirstLab2/FirstLab1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+void main(void)
+{
+    setlocale(LC_ALL, "Ru");
+    int i, k, number;
+    int power(int, int);
+    std::cout << "¬ведите число дл€ возведени€ его в степень: ";
+    std::cin >> number; //ввод числа 
+    for (i = 0;i < 10;i++)
+    {
+        k = power(number, i);
+        std::cout << "i = " << i << ", k = " << k << ", ";
+    }
+}
+int power(int x, int n) // вычисление числа x в степени n
+{
+    int i, p = 1;
+    for (i = 1;i <= n;i++) p *= x;
+    return p;
+}
